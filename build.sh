0da3817d1e1b13cb61f549193c045cdc2562ceb5
henry <yuming.guo@advancegroup.com>
2025-05-12 13:57:10 +0800

    修改Shell脚本及telegram版本号

--- build.sh
+++ build.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
-VERSION=5.15.0
+VERSION=5.14.2
 
-TG=https://github.com/telegramdesktop/tdesktop/releases/download/v$VERSION/tsetup.${VERSION}.tar.xz
+OS_TYPE=$(uname)
 
-sed -r "s#https://github.com/telegramdesktop/tdesktop/releases/download.*tar.xz(.*)#$TG\1#g" Dockerfile
+if [ "$OS_TYPE" == "Linux" ];then
+    sed -ri "s#(.*)/v.*/tsetup.*.tar.xz(.*)#\1/v${VERSION}/tsetup.${VERSION}.tar.xz\2#g" Dockerfile
+elif [ "$OS_TYPE" == "Darwin" ];then
+    sed -i '' -E "s#(.*)/v.*/tsetup.*.tar.xz(.*)#\1/v${VERSION}/tsetup.${VERSION}.tar.xz\2#g" Dockerfile
+else
+    echo "This is an unsupported operating system"
+    exit 1
+fi
 
-docker build -t sassv/telegram:$VERSION .
 
-docker build -t sassv/telegram:latest .
+#docker build -t sassv/telegram:$VERSION .
 
-docker push sassv/telegram:$VERSION
-docker push sassv/telegram:latest
+#docker build -t sassv/telegram:latest .
+
+#docker push sassv/telegram:$VERSION
+#docker push sassv/telegram:latest
